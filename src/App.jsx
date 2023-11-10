5444759686ca6db9c9ab51f0a09a9d1c0bd60df8
jchav1996 <jenniferschavarria@gmail.com>
2023-11-10 13:18:12 -0500

    changes

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react"
+import { Light } from "./light"
+
+export default function App(){
+  const [activeLight, setActiveLight] = useState("red");
+
+useEffect(() => {
+  const interval = setInterval(() => {
+    switch(activeLight) {
+      case "red":
+        setActiveLight("yellow")
+        break
+        case "yellow":
+          setActiveLight("green")
+          break
+          case "green":
+            setActiveLight("red")
+    }
+  }, 3000);
+
+  return () => clearInterval(interval);
+})
+
+
+
+  return (
+    <>
+    <div className="stick"/>
+    <div className="trafficLight">
+      <Light color= "red" opacity={activeLight === "red"? 1:0.4}/>
+      <Light color= "yellow" opacity={activeLight === "yellow"? 1:0.4}/>
+      <Light color= "green" opacity={activeLight === "green"? 1:0.4}/>
+    </div>
+<button>
+  <h1>{activeLight}</h1>
+ON / OFF</button>
+    </>
+  )
+}
---
+++ src/Light.jsx
@@ -0,0 +1,9 @@
+import "./light.css";
+
+export function Light({ color, opacity }) {
+    return (
+        <>
+        <div className="light selected" style={{background: color, opacity: opacity}}/>
+        </>
+    )
+}
